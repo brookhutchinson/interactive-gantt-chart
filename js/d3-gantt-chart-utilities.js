3d3587793294117b5eaa28a8a073c6c33c3a418c
Brook Hutchinson <brookhutchinson@outlook.com>
2017-06-24 10:13:23 -0500

    Setup example, Define css properties inside file named "style.css", Create file named "d3-gantt-chart.js", Create file named "d3-gantt-chart-utilities.js", Add javascript reference to "d3-gantt-chart.js", Add javascript reference to "d3-gantt-chart-utilities.js", Cleanup and format code, Rename variables, Add comments to code

---
+++ js/d3-gantt-chart-utilities.js
@@ -0,0 +1,203 @@
+// data
+var data;
+
+
+// get data from json
+// convert data from json file into javascript object named "data"
+d3.json("data/tasks.json", function(error, data) {
+  // _______________________________________________________________________
+  // handle error
+  if (error) {
+    // display error message on page inside <div>
+    d3.select("#error-message").text("Attention: Problem loading data.")
+      // background color
+      .attr("class", "bg-danger")
+      // font color
+      .style("color", "#fff");
+
+
+    // hide navigation buttons
+    d3.select("#button-container").style("display", "none");
+
+
+    // display error in console
+    return console.log(error);
+  }
+
+
+  // _______________________________________________________________________
+  // success: retrieved data
+
+  // // test: loop through each object in data, write to console
+  // data.forEach(function(d) {
+  //   console.log(d.CustomerInfo.CustomerName);
+  // });
+
+
+  // convert each date string to a date object
+  // loop through each object
+  data.forEach(function(d) {
+    // use dateParser to convert date string to date object
+    var dateParser = d3.timeParse("%Y-%m-%dT%H:%M:%S");
+    d.TaskDuration.StartDate = dateParser(d.TaskDuration.StartDate);
+    d.TaskDuration.EndDate = dateParser(d.TaskDuration.EndDate);
+  });
+
+
+  // create array of customer names
+  var customerNames = data.map(function(d) {
+    return d.CustomerInfo.CustomerName;
+  });
+
+
+  // sort data in ascending order by task end date
+  data.sort(function(a, b) {
+    return a.TaskDuration.EndDate - b.TaskDuration.EndDate;
+  });
+
+
+  // set maximum date
+  var maxDate = data[data.length - 1].TaskDuration.End;
+
+
+  // sort data in ascending order by task start date
+  data.sort(function(a, b) {
+    return a.TaskDuration.StartDate - b.TaskDuration.StartDate;
+  });
+
+
+  // set minimum date
+  var minDate = data[0].TaskDuration.StartDate;
+
+
+  // x-axis tick datetime format
+  var format = "%H:%M";
+
+
+  // set default time domain
+  var timeDomainString = "6months";
+
+
+  // setup gantt chart
+  var gantt = d3.gantt()
+    .svgHeight(450)
+    .svgWidth(800)
+    .customerNames(customerNames)
+    .tickFormat(format);
+
+
+  // change time domain
+  changeTimeDomain(timeDomainString);
+
+
+  // create gantt chart
+  gantt(data);
+
+
+  function changeTimeDomain(timeDomainString) {
+    // datetime domain string corresponds to navigation button that was clicked
+    this.timeDomainString = timeDomainString;
+
+    // set datetime domain based on button that was timeDomainString
+    switch (timeDomainString) {
+      case "1day":
+        // set datetime format
+        format = "%H:%M";
+        // set datetime domain
+        gantt.timeDomain([d3.timeDay.offset(getEndDate(), -1), getEndDate()]);
+        break;
+
+      case "1week":
+        // set datetime format
+        format = "%b %d";
+        // set datetime domain
+        gantt.timeDomain([d3.timeDay.offset(getEndDate(), -7), getEndDate()]);
+        break;
+
+      case "2weeks":
+        // set datetime format
+        format = "%b %d";
+        // set datetime domain
+        gantt.timeDomain([d3.timeDay.offset(getEndDate(), -14), getEndDate()]);
+        break;
+
+      case "1month":
+        // set datetime format
+        format = "%b %d";
+        // set datetime domain
+        gantt.timeDomain([d3.timeDay.offset(getEndDate(), -30), getEndDate()]);
+        break;
+
+      case "3months":
+        // set datetime format
+        format = "%b";
+        // set datetime domain
+        gantt.timeDomain([d3.timeMonth.offset(getEndDate(), -3), getEndDate()]);
+        break;
+
+      case "6months":
+        // set datetime format
+        format = "%b";
+        // set datetime domain
+        gantt.timeDomain([d3.timeMonth.offset(getEndDate(), -6), getEndDate()]);
+        break;
+
+      default:
+        // set datetime format
+        format = "%H:%M"
+    }
+
+
+    // set x-axis tick format
+    gantt.tickFormat(format);
+
+
+    // redraw gantt chart
+    gantt.redraw(data);
+  }
+
+
+  function getEndDate() {
+    // set task end date to today
+    var lastEndDate = Date.now();
+
+    // set task end date
+    if (data.length > 0) {
+      lastEndDate = data[data.length - 1].TaskDuration.EndDate;
+    }
+
+    return lastEndDate;
+  }
+
+
+  // _______________________________________________________________________
+  // navigation button click event handlers used to set x-axis datetime domain
+  $("#oneDay").on("click", function() {
+    changeTimeDomain("1day");
+  });
+
+
+  $("#oneWeek").on("click", function() {
+    changeTimeDomain("1week");
+  });
+
+
+  $("#twoWeeks").on("click", function() {
+    changeTimeDomain("2weeks");
+  });
+
+
+  $("#oneMonth").on("click", function() {
+    changeTimeDomain("1month");
+  });
+
+
+  $("#threeMonths").on("click", function() {
+    changeTimeDomain("3months");
+  });
+
+
+  $("#sixMonths").on("click", function() {
+    changeTimeDomain("6months");
+  });
+});
---
+++ js/d3-gantt-chart.js
@@ -0,0 +1,346 @@
+d3.gantt = function() {
+  // svg dimensions: margin, height, width
+  var svgMargin = { top: 20, right: 40, bottom: 20, left: 150 };
+  var svgHeight = document.body.clientHeight - svgMargin.top - svgMargin.bottom - 5;
+  var svgWidth = document.body.clientWidth - svgMargin.right - svgMargin.left - 5;
+
+
+  // datetime domain mode
+  var FIT_TIME_DOMAIN_MODE = "fit";
+  var FIXED_TIME_DOMAIN_MODE = "fixed";
+  var timeDomainMode = FIXED_TIME_DOMAIN_MODE;
+
+
+  // datetime domain
+  var timeDomainStart = d3.timeDay.offset(new Date(), -3);
+  var timeDomainEnd = d3.timeHour.offset(new Date(), +3);
+
+
+  // customer names displayed on y-axis
+  var customerNames = [];
+
+
+  // x-axis tick format
+  var tickFormat = "%H:%M";
+
+
+  var keyFunction = function(d) {
+    return d.TaskDuration.StartDate + d.CustomerInfo.CustomerName + d.TaskDuration.EndDate;
+  };
+
+
+  var rectTransform = function(d) {
+    return "translate(" + x(d.TaskDuration.StartDate) + "," + y(d.CustomerInfo.CustomerName) + ")";
+  };
+
+
+  var x;
+  var y;
+  var xAxis;
+  var yAxis;
+
+
+  // setup x-axis and y-axis
+  setupAxis();
+
+
+  var setupTimeDomain = function() {
+    if (timeDomainMode === FIT_TIME_DOMAIN_MODE) {
+      if (data === undefined || data.length < 1) {
+        timeDomainStart = d3.timeDay.offset(new Date(), -3);
+        timeDomainEnd = d3.timeHour.offset(new Date(), +3);
+        return;
+      }
+
+
+      // sort data in ascending order by task end date
+      data.sort(function(a, b) {
+        return a.TaskDuration.EndDate - b.TaskDuration.EndDate;
+      });
+
+
+      // set time domain end date
+      timeDomainEnd = data[data.length - 1].TaskDuration.EndDate;
+
+
+      // sort data in ascending order by task start date
+      data.sort(function(a, b) {
+        return a.TaskDuration.StartDate - b.TaskDuration.StartDate;
+      });
+
+
+      // set time domain start date
+      timeDomainStart = data[0].TaskDuration.StartDate;
+    }
+  };
+
+
+  function setupAxis() {
+    // calculate x-position using scaleTime()
+    x = d3.scaleTime()
+      // input domain
+      .domain([timeDomainStart, timeDomainEnd])
+      // output range
+      .range([0, svgWidth])
+      // clamp
+      .clamp(true);
+
+
+    // calculate y-position using scaleBand()
+    y = d3.scaleBand()
+      // input domain
+      .domain(customerNames)
+      // output range
+      .range([0, svgHeight - svgMargin.top - svgMargin.bottom])
+      // padding
+      .padding(0.1);
+
+
+    // x-axis
+    xAxis = d3.axisBottom().scale(x)
+      // tick format
+      .tickFormat(d3.timeFormat(tickFormat))
+      // tick size
+      .tickSize(8)
+      // tick padding
+      .tickPadding(8)
+      // number of ticks
+      .ticks(6);
+
+
+    // y-axis
+    yAxis = d3.axisLeft().scale(y)
+      // tick size
+      .tickSize(0);
+  };
+
+
+  function gantt(data) {
+    // setup datetime domain
+    setupTimeDomain();
+
+
+    // setup x-axis and setup y-axis
+    setupAxis();
+
+
+    // create <svg> element inside svg container
+    var svg = d3.select("#svg-container").append("svg")
+      // apply css
+      .attr("class", "chart")
+      // <svg> width
+      .attr("width", svgWidth + svgMargin.left + svgMargin.right)
+      // <svg> height
+      .attr("height", svgHeight + svgMargin.top + svgMargin.bottom)
+
+
+      // create <g> element
+      .append("g")
+        // apply class
+        .attr("class", "gantt-chart")
+        // <g> width
+        .attr("width", svgWidth + svgMargin.left + svgMargin.right)
+        // <g> height
+        .attr("height", svgHeight + svgMargin.top + svgMargin.bottom)
+        // transform
+        .attr("transform", "translate(" + svgMargin.left + ", " + svgMargin.top + ")");
+
+
+    // make selection of <rect> elements
+    svg.selectAll(".chart")
+      // bind data
+      .data(data, keyFunction)
+      // create placeholder for each missing <rect>
+      .enter()
+        // create <rect>
+        .append("rect")
+          // rx-position
+          .attr("rx", 5)
+          // ry-position
+          .attr("ry", 5)
+          // fill color
+          .attr("fill", "steelblue")
+          // y-position
+          .attr("y", 0)
+          // transform
+          .attr("transform", rectTransform)
+          // height: use bandwidth scale to calculate height
+          .attr("height", function(d) {
+            return y.bandwidth();
+          })
+          // width
+          .attr("width", function(d) {
+            return ( x(d.TaskDuration.EndDate) - x(d.TaskDuration.StartDate) );
+          });
+
+
+    // create <g> element for x-axis
+    svg.append("g")
+      // apply css class
+      .attr("class", "x axis")
+      // move position of x-axis
+      .attr("transform", "translate(0, " + (svgHeight - svgMargin.top - svgMargin.bottom) + ")")
+      // transition
+      .transition()
+        // draw x-axis
+        .call(xAxis);
+
+
+    // create <g> element for y-axis
+    svg.append("g")
+      // apply css class
+      .attr("class", "y axis")
+      // transition
+      .transition()
+        // draw y-axis
+        .call(yAxis);
+
+
+    return gantt;
+  };
+
+
+  gantt.redraw = function(data) {
+    // setup datetime domain
+    setupTimeDomain();
+
+
+    // setup x-axis and setup y-axis
+    setupAxis();
+
+
+    // make selection of svg
+    var svg = d3.select("svg");
+
+
+    // make selection of gantt chart group
+    var ganttChartGroup = svg.select(".gantt-chart");
+
+
+    // create <rect> elements inside <svg> element
+    var rect = ganttChartGroup.selectAll("rect")
+      // bind data item to <rect>
+      .data(data, keyFunction);
+
+    // create placeholder for each missing <rect>
+    rect.enter()
+      // insert new <rect> element
+      .insert("rect", ":first-child")
+        // rx-position
+        .attr("rx", 5)
+        // ry-position
+        .attr("ry", 5)
+        // fill color
+        .attr("fill", "steelblue")
+        // transition
+        .transition()
+        // y-position
+        .attr("y", 0)
+        // move position of <rect> element
+        .attr("transform", rectTransform)
+        // height: use bandwidth scale to calculate height
+        .attr("height", function(d) {
+          return y.bandwidth();
+        })
+        // width
+        .attr("width", function(d) {
+          return (x(d.TaskDuration.EndDate) - x(d.TaskDuration.StartDate));
+        });
+
+
+    // merge <rect> element
+    rect.merge(rect).transition()
+      // move position of <rect> element
+      .attr("transform", rectTransform)
+      // height
+      .attr("height", function(d) {
+        return y.bandwidth();
+      })
+      // width
+      .attr("width", function(d) {
+        return ( x(d.TaskDuration.EndDate) - x(d.TaskDuration.StartDate) );
+      });
+
+
+    // remove <rect> elements
+    rect.exit().remove();
+
+
+    // make selection of x-axis
+    svg.select(".x")
+      // move position of x-axis
+      .transition()
+        // draw x-axis
+        .call(xAxis);
+
+
+    // make selection of y-axis
+    svg.select(".y")
+      // move position of y-axis
+      .transition()
+        // draw y-axis
+        .call(yAxis);
+
+
+    return gantt;
+  };
+
+
+  gantt.svgMargin = function(value) {
+    if (!arguments.length) return svgMargin;
+
+    svgMargin = value;
+    return gantt;
+  };
+
+
+  gantt.timeDomain = function(value) {
+    if (!arguments.length) return [timeDomainStart, timeDomainEnd];
+
+    timeDomainStart = +value[0], timeDomainEnd = +value[1];
+    return gantt;
+  };
+
+
+  gantt.timeDomainMode = function(value) {
+    if (!arguments.length) return timeDomainMode;
+
+    timeDomainMode = value;
+    return gantt;
+  };
+
+
+  gantt.customerNames = function(value) {
+    if (!arguments.length) return customerNames;
+
+    customerNames = value;
+    return gantt;
+  };
+
+
+  gantt.svgWidth = function(value) {
+    if (!arguments.length) return svgWidth;
+
+    svgWidth = +value;
+    return gantt;
+  };
+
+
+  gantt.svgHeight = function(value) {
+    if (!arguments.length) return svgHeight;
+
+    svgHeight = +value;
+    return gantt;
+  };
+
+
+  gantt.tickFormat = function(value) {
+    if (!arguments.length) return tickFormat;
+
+    tickFormat = value;
+    return gantt;
+  };
+
+
+  return gantt;
+};
